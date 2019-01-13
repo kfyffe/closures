aa6ea04a36833576e2594662824b612feb83a98c
Kristen <kristen.fyffe@gmail.com>
2019-01-13 02:56:14 +0000

    Exercise 1 - Loaded Die: created function that returnsa function returning counted values from given array

---
+++ loaded-dice.js
@@ -0,0 +1,17 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var roll = 0;
+
+  return function() {
+    roll++;
+    return list[roll -1];
+
+  }
+ console.log(roll[i]);
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
